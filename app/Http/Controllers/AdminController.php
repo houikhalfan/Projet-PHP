44fbc949e913ea02278d2d5d757af366439b2e6f
houikhalfan <h.ouikhalfan@gmail.com>
2025-04-05 17:52:04 +0000

    3

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -8,14 +8,16 @@ use App\Models\Category;
 class AdminController extends Controller
 {
     public function view_category(){
-        return view('admin.category');
+        $data = Category::all();
+        return view('admin.category', compact('data'));
     }
     
     public function add_category(Request $request){
         $category= new Category;
         $category->category_name = $request->category;
         $category->save();
+        toastr()->closeButton()->addSuccess('Category added Successfully');
         return redirect()->back();
-        
+
     }
 }
