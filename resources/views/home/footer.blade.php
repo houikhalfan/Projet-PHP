cc07cb74eecd9aa10a8d137260e85636b16f085e
houikhalfan <h.ouikhalfan@gmail.com>
2025-04-05 15:58:26 +0000

    again

---
+++ resources/views/home/footer.blade.php
@@ -0,0 +1,89 @@
+<section class="info_section layout_padding2-top">
+    <div class="social_container">
+      <div class="social_box">
+        <a href="">
+          <i class="fa fa-facebook" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-twitter" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        <a href="">
+          <i class="fa fa-youtube" aria-hidden="true"></i>
+        </a>
+      </div>
+    </div>
+    <div class="info_container">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              À PROPOS
+            </h6>
+            <p>
+              Delicia Cakes est une boutique en ligne spécialisée dans la confection de gâteaux artisanaux et gourmands. Nous utilisons des ingrédients de qualité pour vous offrir des créations savoureuses et uniques, parfaites pour toutes les occasions.
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="info_form">
+              <h5>
+                Newsletter
+              </h5>
+              <form action="#">
+                <input type="email" placeholder="Entrez votre email">
+                <button>
+                  S'abonner
+                </button>
+              </form>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              BESOIN D'AIDE ?
+            </h6>
+            <p>
+              Vous avez des questions sur nos produits ou besoin de conseils pour choisir le gâteau parfait ? Notre équipe est à votre disposition pour vous accompagner et répondre à toutes vos demandes.
+            </p>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <h6>
+              CONTACTEZ-NOUS
+            </h6>
+            <div class="info_link-box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span> Agadir, Morocco </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>+212 700000000</span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span> contact@deliciacakes.com</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- footer section -->
+    <footer class="footer_section">
+      <div class="container">
+        <p>
+          &copy; <span id="displayYear"></span> Tous droits réservés par deliciacakes
+        </p>
+      </div>
+    </footer>
+    <!-- footer section -->
+  </section>
+
+  <!-- end info section -->
+
+  <script src="{{asset('js/jquery-3.4.1.min.js')}}"></script>
+  <script src="{{asset('js/bootstrap.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+  <script src="{{asset('js/custom.js')}}"></script>
---
+++ resources/views/home/slider.blade.php
@@ -0,0 +1,37 @@
+<section class="slider_section">
+  <div class="slider_container">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-md-7">
+                <div class="detail-box"> 
+                  <h1>
+                    Bienvenue Chez <br>
+                    Delicia Cakes
+                  </h1>
+                  <p>
+                    Découvrez un monde de douceurs avec Delicia Cakes !
+                     Nous vous proposons une large sélection de gâteaux faits maison, 
+                     préparés avec des ingrédients de qualité pour satisfaire toutes vos envies sucrées. 
+                     Que ce soit pour un anniversaire, un événement spécial ou simplement pour se faire plaisir, 
+                     nos créations gourmandes raviront vos papilles.
+                  </p>
+                  <a href="contact.html">
+                    Nous Contacter
+                  </a>
+                </div>
+              </div>
+              <div class="col-md-5 ">
+                <div class="img-box">
+                  <img style="width:600px" src="images/image3.jpg" alt="Gâteau délicieux" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
