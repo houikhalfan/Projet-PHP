64a3c04f423825633eab8c79f68dd90cad6fa59d
houikhalfan <h.ouikhalfan@gmail.com>
2025-04-05 16:59:00 +0000

    2

--- resources/views/admin/category.blade.php
+++ resources/views/admin/category.blade.php
@@ -2,6 +2,21 @@
 <html>
   <head> 
   @include('admin.css')  
+  <style type="text/css">
+    input[type='text']
+    {
+      width: 400px;
+      height: 50px
+    }
+    .div_deg
+    {
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      margin:30px;
+
+    }
+  </style>
   </head>
   <body>
     @include('admin.header')
@@ -9,9 +24,18 @@
       <div class="page-content">
         <div class="page-header">
           <div class="container-fluid">
-
-        
-        @include('admin.body')   
+          <h1 style="color: white;">Add Category</h1>
+   <div class="div_deg">
+  
+          <form action="{{url('add_category')}}" method="post">
+            @csrf
+    <div>
+    <input type="text" name="category">
+ 
+      <input class="btn btn-primary" type="submit" value="Add Category">
+    </div>
+    </form>  
+    </div>  
 </div> 
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,4 @@ require __DIR__.'/auth.php';
 
 route::get('admin/dashboard',[HomeController::class,'index'])->middleware(['auth','admin']);
 route::get('view_category',[AdminController::class,'view_category'])->middleware(['auth','admin']);
+route::post('add_category',[AdminController::class,'add_category'])->middleware(['auth','admin']);
